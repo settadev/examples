48842d85fa6ae6657248ab3d3308677c769583f6
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-17 21:13:28 -0500

    added sections to stream diffusion example

---
+++ stream_diffusion_img_to_img/setta_files/setta_export/with_variants/img2img/img_code@wily_red.py
@@ -0,0 +1,45 @@
+from setta.tasks.fns import SettaInMemoryFn
+import base64
+import io
+from PIL import Image
+
+$SETTA_GENERATED_PYTHON
+
+def base64_to_pil(base64_string: str) -> Image.Image:
+    # Remove data URL prefix if present
+    if "base64," in base64_string:
+        base64_string = base64_string.split("base64,")[1]
+
+    # Convert base64 to PIL Image
+    img_bytes = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(img_bytes))
+
+    return image
+
+
+def pil_to_base64(pil_image: Image.Image, format="PNG") -> str:
+    buffered = io.BytesIO()
+    pil_image.save(buffered, format=format)
+    return base64.b64encode(buffered.getvalue()).decode()
+
+
+def drawing_to_img(project):
+    x = base64_to_pil(project["input_image"]["drawing"])
+
+    image_tensor = stream.preprocess_image(x)
+
+    for _ in range(stream.batch_size - 1):
+        stream(image=image_tensor)
+
+    output_image = stream(image=image_tensor)
+
+    output = pil_to_base64(output_image)
+    return [
+        {
+            "name": "output" + "_" + $stream$version + "_" + $stream_prepared$version,
+            "type": "img",
+            "value": output,
+        }
+    ]
+
+fn = SettaInMemoryFn(fn=drawing_to_img)
