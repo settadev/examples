8055a1340ab2b1281eee39182a295450bd4b9468
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-17 20:53:24 -0500

    updated install.sh

--- stream_diffusion_img_to_img/install.sh
+++ stream_diffusion_img_to_img/install.sh
@@ -1,9 +1,6 @@
 # Steps are from https://github.com/cumulo-autumn/StreamDiffusion
 
-# Exit immediately if any command fails
-set -e
-
-pip3 install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu118
-pip install git+https://github.com/cumulo-autumn/StreamDiffusion.git@main#egg=streamdiffusion[tensorrt]
+pip install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu118 && \
+pip install git+https://github.com/cumulo-autumn/StreamDiffusion.git@main#egg=streamdiffusion[tensorrt] && \
+pip install huggingface-hub==0.25.2 transformers==4.48.3 "numpy<2.0" setta==0.0.3.dev13 && \
 python -m streamdiffusion.tools.install-tensorrt
-pip install setta==0.0.3.dev13
