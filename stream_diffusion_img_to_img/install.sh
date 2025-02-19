349aa6b907484c889bae0c32d0b6b36f50363954
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-19 18:23:57 -0500

    use sd-turbo

--- stream_diffusion_img_to_img/install.sh
+++ stream_diffusion_img_to_img/install.sh
@@ -1,6 +1,6 @@
 # Steps are from https://github.com/cumulo-autumn/StreamDiffusion
 
-pip install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu118 && \
+pip install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu121 && \
 pip install git+https://github.com/cumulo-autumn/StreamDiffusion.git@main#egg=streamdiffusion[tensorrt] && \
-pip install huggingface-hub==0.25.2 transformers==4.48.3 "numpy<2.0" setta==0.0.3 && \
+pip install huggingface-hub==0.25.2 transformers==4.48.3 "numpy<2.0" setta==0.0.4.dev1 && \
 python -m streamdiffusion.tools.install-tensorrt
