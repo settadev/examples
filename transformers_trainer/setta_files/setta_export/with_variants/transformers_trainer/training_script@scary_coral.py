65ae441ed9d46808fd1c5e4f16f88ce814418dde
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-20 21:32:39 -0500

    added trainer/model stuff and deepspeed config

--- transformers_trainer/setta_files/setta_export/with_variants/transformers_trainer/training_script@scary_coral.py
+++ transformers_trainer/setta_files/setta_export/with_variants/transformers_trainer/training_script@scary_coral.py
@@ -1,6 +1,17 @@
+import numpy as np
+
 def get_tokenize_function(tokenizer, **kwargs):
     def tokenize_function(examples):
         return tokenizer(examples["text"], **kwargs)
     return tokenize_function
 
+def get_compute_metrics(metric):
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+    return compute_metrics
+
 $SETTA_GENERATED_PYTHON
+
+training_setup["trainer"].train()
