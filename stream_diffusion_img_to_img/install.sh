59a2f524450e090dd4c33ec82493b1397d0686b9
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-27 18:25:56 -0500

    update dependencies and gitignore

--- stream_diffusion_img_to_img/install.sh
+++ stream_diffusion_img_to_img/install.sh
@@ -2,5 +2,5 @@
 
 pip install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu121 && \
 pip install git+https://github.com/cumulo-autumn/StreamDiffusion.git@main#egg=streamdiffusion[tensorrt] && \
-pip install huggingface-hub==0.25.2 transformers==4.48.3 "numpy<2.0" setta==0.0.7 && \
+pip install huggingface-hub==0.25.2 transformers==4.48.3 "numpy<2.0" setta==0.0.9.dev2 && \
 python -m streamdiffusion.tools.install-tensorrt
