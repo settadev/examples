78eed78470630e63a64c5c9498297da2aea73a5c
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-04 23:10:41 -0500

    added redux model stuff

--- bfl_flux_fill/setta_files/setta_export/with_variants/bfl_flux_fill/passing_moccasin@actual_amethyst.py
+++ bfl_flux_fill/setta_files/setta_export/with_variants/bfl_flux_fill/passing_moccasin@actual_amethyst.py
@@ -8,6 +8,8 @@ from $base64_utils$import_path import (
 $SETTA_GENERATED_PYTHON
 
 inpainter = inpainter.to("cuda")
+adapter = adapter.to("cuda")
+base_model = base_model.to("cuda")
 
 
 def inpaint_fn(p):
@@ -27,4 +29,15 @@ def inpaint_fn(p):
     return [{"name": "inpainted", "type": "img", "value": output}]
 
 
+def redux_fn(p):
+    image = p["inpainted"]["image"]
+    image = base64_to_pil(image)
+    adapter_output = adapter(image)
+    images = base_model(**base_model_args, **adapter_output).images
+    output = pil_to_base64(images[0])
+    return [{"name": "variation", "type": "img", "value": output}]
+
+
+
 fn = SettaInMemoryFn(fn=inpaint_fn, dependencies=[])
+fn2 = SettaInMemoryFn(fn=redux_fn, dependencies=[])
