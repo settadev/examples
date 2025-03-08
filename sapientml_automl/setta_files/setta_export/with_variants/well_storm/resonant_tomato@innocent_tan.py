8b377b03d4d9d1036ab734e491cfcceb05928a50
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-08 18:57:29 -0500

    use some sliders and switches

--- sapientml_automl/setta_files/setta_export/with_variants/well_storm/resonant_tomato@innocent_tan.py
+++ sapientml_automl/setta_files/setta_export/with_variants/well_storm/resonant_tomato@innocent_tan.py
@@ -1,11 +1,15 @@
 import pandas as pd
 from sklearn.metrics import r2_score
 
+def evaluate(data):
+    y_true = data[target_column].reset_index(drop=True)
+    data.drop([target_column], axis=1, inplace=True)
+    y_pred = sml.predict(data)
+    y_pred = y_pred[target_column].rename(f"{target_column}_pred")
+    pd.concat([y_pred, y_true], axis=1)
+    print(f"R2 score: {r2_score(y_true, y_pred)}")
+
 $SETTA_GENERATED_PYTHON
 
-y_true = test_data[target_column].reset_index(drop=True)
-test_data.drop([target_column], axis=1, inplace=True)
-y_pred = sml.predict(test_data)
-y_pred = y_pred[target_column].rename(f"{target_column}_pred")
-pd.concat([y_pred, y_true], axis=1)
-print(f"R2 score: {r2_score(y_true, y_pred)}")
+evaluate(test_data)
+
