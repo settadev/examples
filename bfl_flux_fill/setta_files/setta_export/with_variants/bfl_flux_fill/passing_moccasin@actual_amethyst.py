65e2ff993aa0c661e992fe81e8d80139359e4195
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-07 19:55:31 -0500

    export db

--- bfl_flux_fill/setta_files/setta_export/with_variants/bfl_flux_fill/passing_moccasin@actual_amethyst.py
+++ bfl_flux_fill/setta_files/setta_export/with_variants/bfl_flux_fill/passing_moccasin@actual_amethyst.py
@@ -11,24 +11,8 @@ $SETTA_GENERATED_PYTHON
 
 model = model.to("cuda")
 
-first_call = True
-original_size = None
 
-
-def save_and_return_image(image, output_name):
-    output_path = f"output_imgs/{output_name}.png"
-    image.save(output_path)
-    return [
-        {
-            "name": output_name,
-            "type": "img",
-            "path": output_path,
-            "value": pil_to_base64(image),
-        }
-    ]
-
-
-def inpaint_or_outpaint(layers, prompt, output_name):
+def inpaint_or_outpaint(layers, prompt, output_name, image_size=None):
     layers = layers[1:]
     image = base64_to_pil(layers[0])
     width, height = image.size
@@ -45,35 +29,31 @@ def inpaint_or_outpaint(layers, prompt, output_name):
         generator=torch.Generator("cpu").manual_seed(0),
     ).images[0]
 
-    return save_and_return_image(image, output_name)
-
-
-def iterative_outpaint(layers, prompt, output_name):
-    global first_call, original_size
-    if first_call:
-        first_call = False
-        image = Image.open("input_imgs/bfl_example_5.jpg")
-        original_size = image.size
-        return save_and_return_image(image, output_name)
-    return inpaint_or_outpaint(layers, prompt, output_name)
+    output_path = f"output_imgs/{output_name}.png"
+    image.save(output_path)
+    return [
+        {
+            "name": output_name,
+            "type": "img",
+            "path": output_path,
+            "value": pil_to_base64(image),
+        }
+    ]
 
 
 def get_inpaint_output_fn(
-    section_name, prompt_name, output_name, do_iterative_outpaint
+    section_name, prompt_name, output_name
 ):
     def fn(p):
         drawing = p[section_name]["drawing"]
-        args = [drawing, p[prompt_name]["text"], output_name]
-        if do_iterative_outpaint:
-            return iterative_outpaint(*args)
-        return inpaint_or_outpaint(*args)
+        return inpaint_or_outpaint(drawing, p[prompt_name]["text"], output_name)
 
     return fn
 
 
-def create_in_memory_fn(idx, do_iterative_outpaint=False):
+def create_in_memory_fn(idx):
     fn = get_inpaint_output_fn(
-        f"input_{idx}", f"prompt_{idx}", f"output_{idx}", do_iterative_outpaint
+        f"input_{idx}", f"prompt_{idx}", f"output_{idx}"
     )
     return SettaInMemoryFn(fn=fn, dependencies=[f"input_{idx}['drawing']"])
 
@@ -81,4 +61,4 @@ def create_in_memory_fn(idx, do_iterative_outpaint=False):
 # fn1 = create_in_memory_fn(1)
 # fn2 = create_in_memory_fn(2)
 # fn3 = create_in_memory_fn(3)
-fn3 = create_in_memory_fn(4, True)
+fn3 = create_in_memory_fn(4)
