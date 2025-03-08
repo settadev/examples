d8493d3c26216acfd20a3ef2e3b0ca995a54ee70
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-08 16:51:41 -0500

    use different dataset

--- sapientml_automl/setta_files/setta_export/with_variants/well_storm/resonant_tomato@innocent_tan.py
+++ sapientml_automl/setta_files/setta_export/with_variants/well_storm/resonant_tomato@innocent_tan.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from sklearn.metrics import f1_score
+from sklearn.metrics import r2_score
 
 $SETTA_GENERATED_PYTHON
 
-test_data = split_data[1]
 y_true = test_data[target_column].reset_index(drop=True)
 test_data.drop([target_column], axis=1, inplace=True)
 y_pred = sml.predict(test_data)
 y_pred = y_pred[target_column].rename(f"{target_column}_pred")
-print(f"F1 score: {f1_score(y_true, y_pred)}")
+pd.concat([y_pred, y_true], axis=1)
+print(f"R2 score: {r2_score(y_true, y_pred)}")
