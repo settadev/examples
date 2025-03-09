7f2b21c4dd28692c4eb23b047e84a9108f9d80a5
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-08 21:00:21 -0500

    add score_fn_info

--- sapientml_automl/setta_files/setta_export/with_variants/insurance/resonant_tomato@innocent_tan.py
+++ sapientml_automl/setta_files/setta_export/with_variants/insurance/resonant_tomato@innocent_tan.py
@@ -1,23 +1,19 @@
 import pandas as pd
 from sklearn.metrics import r2_score, f1_score
 
-def evaluate(sml, data, target_column):
+def evaluate(sml, data, target_column, score_fn_info):
     y_true = data[target_column].reset_index(drop=True)
     data.drop([target_column], axis=1, inplace=True)
     y_pred = sml.predict(data)
     y_pred = y_pred[target_column].rename(f"{target_column}_pred")
     pd.concat([y_pred, y_true], axis=1)
-    if sml.task.adaptation_metric == "r2":
-        score_fn = r2_score
-    else:
-        score_fn = f1_score
-    print(f"Score: {score_fn(y_true, y_pred)}")
+    score_fn = score_fn_info.pop("fn")
+    print(f"Score: {score_fn(y_true, y_pred, **score_fn_info)}")
 
-def train():
+def train_and_evaluate():
     version = ($Global Variables$version).split("@")[1]
     $SETTA_GENERATED_PYTHON
-    return sml, test_data, target_column
+    evaluate(sml, test_data, target_column, score_fn_info)
 
-sml, test_data, target_column = train()
-evaluate(sml, test_data, target_column)
 
+train_and_evaluate()
